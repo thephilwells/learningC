166567580128cb2755195c18760f4ed1c6f5dbe8
Phil Wells <philwells@thephilwells.com>
2016-12-19 06:58:44 -0500

    reverse order

--- temp_for.c
+++ temp_for.c
@@ -7,6 +7,6 @@ main()
 	int cels;
 	
 	printf("CELS | FAHRENHEIT\n");
-	for (cels = 0; cels <= 300; cels = cels + 20)
+	for (cels = 300; cels >= 0; cels = cels - 20)
 		printf("%4i %9.0f\n", cels, (9.0/5.0)*cels+32);
 }
